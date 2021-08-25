e641e94b4115a05f0057d52204e5ab8ef89c622e
Davisralves <davialves324@gmail.com>
2021-08-25 16:45:02 -0300

    adicionando função printNewsByCategory()

--- js/script.js
+++ js/script.js
@@ -4,16 +4,14 @@ import { object } from '../js/object.js';
 console.log(apiToken);
 const url = ('https://gnews.io/api/v4/top-headlines?&country=br&token=');
 
-const getJson = async (parameter) => {
-  if (!parameter) parameter = '';
-  const json = await fetch(`${url}${parameter}${apiToken}`);
-  return await json.json();
+const getJson = async (parameter = '') => {
+  const promisse = await fetch(`${url}${parameter}${apiToken}`);
+  return await promisse.json();
 };
 
 const acess = (event) => window.location.href = event.path[0].name;
 
 const printNews = (news, section) => {
-  console.log(news);
   news.forEach((New) => {
     const { title, description, image, url } = New;
     const div = document.createElement('div');
@@ -31,6 +29,14 @@ const printNews = (news, section) => {
     div.appendChild(img);
     div.appendChild(p);
   });
+};
+
+const printNewsByCategory = async (category, event) => {
+  if(!category) {
+    category = `&"${event.innerText}"`;
+  } else category = `&"${category}"`;
+  const news = await getJson(category);
+  printNews(news.articles, document.querySelector('section'));
 };
 
 const main = async () => {
