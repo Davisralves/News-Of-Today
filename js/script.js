7c07c9136e551aa354306a90bc2177e5344c184c
Davisralves <davialves324@gmail.com>
2021-08-25 13:05:31 -0300

    finalizando printNews

--- js/script.js
+++ js/script.js
@@ -4,12 +4,29 @@ const url = ('https://gnews.io/api/v4/top-headlines?&country=br&token=');
 
 const getJson = async () => {
   const json = await fetch(`${url}${apiToken}`);
-  return json.json();
+  return await json.json();
+};
+
+const printNews = (news, section) => {
+  news.forEach((New) => {
+    const { title, description, image } = New;
+    const div = document.createElement('div');
+    const h4 = document.createElement('h4');
+    h4.innerText = title;
+    const img = document.createElement('img');
+    img.src = image;
+    const p = document.createElement('p');
+    p.innerText = description;
+    section.appendChild(div);
+    div.appendChild(h4);
+    div.appendChild(img);
+    div.appendChild(p);
+  });
 };
 
 const main = async () => {
   const news = await getJson();
-  console.log(news);
+  printNews(news.articles, document.querySelector('section'));
 };
 
 window.onload = async function() {
