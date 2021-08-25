f969f3ca71aab1e544bbc6fe804555d3169a2f15
Davisralves <davialves324@gmail.com>
2021-08-25 12:01:48 -0300

    adicionando key e formatando arquivos

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-const { apiToken } = require('../../key/apiKey');
-
-const url = 'https://gnews.io/api/v4/top-headlines?token='
+import { apiToken } from '../key/apiKey.js';
+console.log(apiToken);
+const url = ('https://gnews.io/api/v4/top-headlines?&country=br&token=');
 
 const getJson = async () => {
   const json = await fetch(`${url}${apiToken}`);
   return json.json();
-}
+};
+
+// console.log(`${url}${apiToken}`);
 
 const news =  getJson().then((promisse) => console.log(promisse));
