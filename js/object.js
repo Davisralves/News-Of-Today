3e96aa52a7fd5d4b50ad27004e61f6edeecca1de
Davisralves <davialves324@gmail.com>
2021-08-25 16:07:26 -0300

    adicionando link da noticia a imagem

---
+++ js/object.js
@@ -0,0 +1,124 @@
+
+export const object = 
+[
+  {
+      "title": "Em conversa, Xi e Putin prometem trabalhar por 'governo inclusivo' no Afeganistão",
+      "description": "Presidentes da China e da Rússia conversaram pela primeira vez desde que Talibã retomou Cabul; Putin envia aviões para evacuar russos de Cabul",
+      "content": "PEQUIM E MOSCOU — Os presidentes da China, Xi Jinping, e da Rússia, Vladimir Putin, conversaram nesta quarta-feira, em seu primeiro telefonema desde que o Talibã retomou o poder no Afeganistão, no último dia 15. Eles concordaram em trabalhar em conju... [3990 chars]",
+      "url": "https://oglobo.globo.com/mundo/em-conversa-xi-putin-prometem-trabalhar-por-governo-inclusivo-no-afeganistao-25170215",
+      "image": "https://ogimg.infoglobo.com.br/in/25170220-d62-1c7/FT1086A/760/83063968_Russian-President-Vladimir-Putin-R-shakes-hands-with-Chinese-President-Xi-Jinping-durin.jpg",
+      "publishedAt": "2021-08-25T16:07:50Z",
+      "source": {
+          "name": "Jornal O Globo",
+          "url": "https://oglobo.globo.com"
+      }
+  },
+  {
+      "title": "Homem que consta como sócio do FIB Bank acusa empresa de falsificar sua assinatura",
+      "description": "Os senadores da CPI da Covid reproduziram há pouco o áudio de um homem que consta como sócio fundador do FIB Bank. O presidente da empresa, Roberto Pereira Ramos Júnior, presta depoimento neste momento à comissão parlamentar...",
+      "content": "Senadores da CPI reproduziram um áudio em que Geraldo Machado diz que foi surpreendido com a informação de que consta no quadro societário do FIB Bank\n×\nOs senadores da CPI da Covid reproduziram há pouco o áudio de um homem que consta como sócio fund... [1292 chars]",
+      "url": "https://www.oantagonista.com/brasil/homem-que-consta-como-socio-do-fib-bank-acusa-empresa-de-falsificar-sua-assinatura/",
+      "image": "https://cdn.oantagonista.com/uploads/2021/08/Diretor-do-FIB-BanK-Roberto-Pereira-Ramos-Junior-2.jpg",
+      "publishedAt": "2021-08-25T16:07:24Z",
+      "source": {
+          "name": "O Antagonista",
+          "url": "https://www.oantagonista.com"
+      }
+  },
+  {
+      "title": "Órgãos públicos federais deverão reduzir consumo de energia de 10% a 20%, define decreto",
+      "description": "País enfrenta pior crise hídrica dos últimos 91 anos. Nesta terça (24), Comitê de Monitoramento recomendou mais medidas para preservar hidrelétricas em razão da 'relevante piora'.",
+      "content": "O governo federal publicou nesta quarta-feira (25) um decreto no qual determinou que os órgãos públicos federais deverão reduzir o consumo de energia de 10% a 20% entre setembro de 2021 e abril de 2022 .\nO decreto é assinado pelo presidente Jair Bols... [2187 chars]",
+      "url": "https://g1.globo.com/politica/noticia/2021/08/25/orgaos-publicos-federais-deverao-reduzir-consumo-de-energia-de-10percent-a-20percent-define-decreto.ghtml",
+      "image": "https://s2.glbimg.com/i4moe4_tJrf5cNfn9WPp4Ip6EcY=/1200x/smart/filters:cover():strip_icc()/s04.video.glbimg.com/x720/9797947.jpg",
+      "publishedAt": "2021-08-25T16:02:56Z",
+      "source": {
+          "name": "G1",
+          "url": "https://g1.globo.com"
+      }
+  },
+  {
+      "title": "Ministro do STJ suspende análise de denúncia contra Flávio Bolsonaro no caso das rachadinhas",
+      "description": "Ministro João Otávio de Noronha considerou que nova versão da denúncia foi embasada em provas já declaradas nulas.",
+      "content": "O ministro João Otávio de Noronha, do Superior Tribunal de Justiça (STJ), suspendeu a análise da nova denúncia oferecida pelo Ministério Público do Rio de Janeiro contra o senador Flávio Bolsonaro (Republicanos-RJ) pelo esquema da rachadinha.\nNoronha... [1799 chars]",
+      "url": "https://g1.globo.com/politica/noticia/2021/08/25/ministro-do-stj-suspende-analise-de-denuncia-contra-flavio-bolsonaro-no-caso-das-rachadinhas.ghtml",
+      "image": "https://s.glbimg.com/jo/g1/static/live/imagens/img_facebook.png",
+      "publishedAt": "2021-08-25T15:36:46Z",
+      "source": {
+          "name": "G1",
+          "url": "https://g1.globo.com"
+      }
+  },
+  {
+      "title": "Órgãos federais deverão reduzir consumo de energia em pelo menos 10%, determina Bolsonaro",
+      "description": "Medida tomada por conta da crise hídrica não vale para estatais",
+      "content": "BRASÍLIA — O presidente Jair Bolsonaro editou nesta quarta-feira um decreto que determina a redução do consumo de energia elétrica nos meses de setembro de 2021 a abril de 2022 em todos os órgãoos da administração direta, autárquica e fundacional. Os... [3800 chars]",
+      "url": "https://oglobo.globo.com/economia/orgaos-federais-deverao-reduzir-consumo-de-energia-em-pelo-menos-10-determina-bolsonaro-1-25170070",
+      "image": "https://ogimg.infoglobo.com.br/economia/25169601-5a1-c8a/FT1086A/760/94925445_A-Barragem-de-Itaipu-e-uma-enorme-barragem-hidroeletrica-no-rio-Parana-entre-o-Brasil-e-o-P.jpg",
+      "publishedAt": "2021-08-25T15:02:59Z",
+      "source": {
+          "name": "Jornal O Globo",
+          "url": "https://oglobo.globo.com"
+      }
+  },
+  {
+      "title": "Comandante do Exército manifesta compromisso com \"estabilidade\" no país",
+      "description": "003O comandante do Exérctio, Paulo Sérgio Nogueira, disse há pouco em discurso durante a cerimônia do Dia do Soldado nesta quarta-feira (25) que está comprometido com a estabilidade e com a tranquilidade. \"O momento desta justa homenagem aos soldados, que muito contribuíram...",
+      "content": "Em evento que contou com a participação de Jair Bolsonaro, Paulo Sérgio Nogueira disse que a Força atua em defesa da democracia\n×\nO comandante do Exérctio, Paulo Sérgio Nogueira, disse há pouco, em discurso durante a cerimônia do Dia do Soldado, que ... [601 chars]",
+      "url": "https://www.oantagonista.com/brasil/comandante-do-exercito-manifesta-compromisso-com-estabilidade-no-pais/",
+      "image": "https://cdn.oantagonista.com/uploads/2021/03/Marcelo-Camargo-Agencia-Brasil.jpg",
+      "publishedAt": "2021-08-25T14:34:42Z",
+      "source": {
+          "name": "O Antagonista",
+          "url": "https://www.oantagonista.com"
+      }
+  },
+  {
+      "title": "Netflix fará evento global para fãs",
+      "description": "Netflix terá edição global do Tudum, com novidades de filmes e séries para fãs. Evento acontece em setembro",
+      "content": "Depois de organizar duas edições do Festival Tudum aqui no Brasil, a Netflix anunciou nesta quarta-feira (25) que irá ampliar o evento -- agora, internacionalmente, como o Tudum: Um evento mundial para fãs.\nO evento acontece no dia 25 de setembro, de... [935 chars]",
+      "url": "https://www.omelete.com.br/netflix/tudum-netflix-fara-evento-mundial-para-fas",
+      "image": "https://cdn.ome.lt/f_GgGuMFMPylTMTQLQo2JSJ-nIM=/1200x630/smart/extras/conteudos/stranger_things_ZDSJpGQ.jpg",
+      "publishedAt": "2021-08-25T14:00:00Z",
+      "source": {
+          "name": "Omelete",
+          "url": "https://www.omelete.com.br"
+      }
+  },
+  {
+      "title": "A caminho do Corinthians, Roger Guedes se despede do Shandong",
+      "description": "Na noite dessa terça-feira, o Shandong confirmou a rescisão de contrato com Roger Guedes e prestou seu agradecimento ao atacante. Na manhã desta",
+      "content": "Na noite dessa terça-feira, o Shandong confirmou a rescisão de contrato com Roger Guedes e prestou seu agradecimento ao atacante. Na manhã desta quarta-feira, foi a vez do jogador se despedir.\nEm um postagem no Instagram, Roger Guedes demonstrou grat... [2629 chars]",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/a-caminho-do-corinthians-roger-guedes-se-despede-do-shandong/",
+      "image": "https://www.gazetaesportiva.com/wp-content/uploads/imagem/2021/08/25/WhatsApp-Image-2021-08-25-at-00.19.07.jpeg",
+      "publishedAt": "2021-08-25T13:52:35Z",
+      "source": {
+          "name": "Gazeta Esportiva",
+          "url": "https://www.gazetaesportiva.com"
+      }
+  },
+  {
+      "title": "Queiroga anuncia 3ª dose da vacina contra Covid para 15 de setembro",
+      "description": "Ministério também reduzirá o intervalo entre a primeira e a segunda dose de 12 para 8 semanas",
+      "content": "O ministro da Saúde, Marcelo Queiroga, anunciou que a terceira dose da vacina contra o coronavírus começará a ser aplicada em idosos entre 70 e 80 anos e em imunossuprimidos a partir do dia 15 de setembro.\nTodos os imunossuprimidos que já tomaram a s... [1426 chars]",
+      "url": "https://www1.folha.uol.com.br/colunas/monicabergamo/2021/08/queiroga-anuncia-3a-dose-da-vacina-para-15-de-setembro.shtml",
+      "image": "https://f.i.uol.com.br/fotografia/2021/06/22/162438875060d2348e734d3_1624388750_3x2_rt.jpg",
+      "publishedAt": "2021-08-25T12:00:00Z",
+      "source": {
+          "name": "Folha de S.Paulo",
+          "url": "https://www1.folha.uol.com.br"
+      }
+  },
+  {
+      "title": "Inflação está 'dentro do jogo', como disse Guedes? Entenda por que a escalada de preços preocupa",
+      "description": "Ministro da Economia afirma que alta de preços está 'dentro do jogo', mas índices de Estados Unidos e Europa subiram por outros motivos. Crise hídrica, desajuste fiscal, turbulência política e real desvalorizado são características particulares que dão força à inflação brasileira.",
+      "content": "Ministro da Economia, Paulo Guedes: para ele, uma inflação entre 7% e 8% está \"dentro do jogo\". — Foto: REUTERS/Adriano Machado.\n1 de 1 Ministro da Economia, Paulo Guedes: para ele, uma inflação entre 7% e 8% está \"dentro do jogo\". — Foto: REUTERS/Ad... [5206 chars]",
+      "url": "https://g1.globo.com/economia/noticia/2021/08/25/inflacao-esta-ou-nao-dentro-do-jogo-como-disse-guedes-entenda-por-que-a-escalada-de-precos-preocupa.ghtml",
+      "image": "https://s2.glbimg.com/r-MK0ISth1Izcq26JyfceKuLUBw=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/a/3/f9hDddTk22aiPOo7hSrw/2021-03-13t002259z-1-lynxmpeh2c00g-rtroptp-4-brazil-economy-guedes.jpg",
+      "publishedAt": "2021-08-25T11:38:26Z",
+      "source": {
+          "name": "G1",
+          "url": "https://g1.globo.com"
+      }
+  }
+];
--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 import { apiToken } from '../key/apiKey.js';
+import { object } from '../js/object.js';
+
 console.log(apiToken);
 const url = ('https://gnews.io/api/v4/top-headlines?&country=br&token=');
 
-const getJson = async () => {
-  const json = await fetch(`${url}${apiToken}`);
+const getJson = async (parameter) => {
+  if (!parameter) parameter = '';
+  const json = await fetch(`${url}${parameter}${apiToken}`);
   return await json.json();
 };
 
+const acess = (event) => window.location.href = event.path[0].name;
+
 const printNews = (news, section) => {
+  console.log(news);
   news.forEach((New) => {
-    const { title, description, image } = New;
+    const { title, description, image, url } = New;
     const div = document.createElement('div');
     const h4 = document.createElement('h4');
     h4.innerText = title;
     const img = document.createElement('img');
     img.src = image;
+    img.name = url;
+    console.log(img.value);
+    img.addEventListener('click', acess);
     const p = document.createElement('p');
     p.innerText = description;
     section.appendChild(div);
@@ -25,8 +34,9 @@ const printNews = (news, section) => {
 };
 
 const main = async () => {
-  const news = await getJson();
-  printNews(news.articles, document.querySelector('section'));
+  // const news = await getJson();
+  const news = object;
+  printNews(news /*.articles*/, document.querySelector('section'));
 };
 
 window.onload = async function() {
